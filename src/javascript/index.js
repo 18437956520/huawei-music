1ec3fa54ad373649e468fc359c502a1baa8dd620
yyyh1231 <444510796@qq.com>
2019-05-27 13:10:12 +0800

    start js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,15 @@
 import './icons.js'
+
+const $ = selector =>document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
+
+class Player{
+    constructor(node){
+        this.root = typeof node === 'string' ? $(node) : node  
+    }
+
+    bind(){
+
+    }
+}
+
